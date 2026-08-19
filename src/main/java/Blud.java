b6aa7e25c753204ad8ae5844bc179d336386793f
Aw Guangyang Amos <aa.gyang88@gmail.com>
2026-08-19 10:54:02 +0800

    Updated Blud.java with simple user greeting and exit

--- src/main/java/Blud.java
+++ src/main/java/Blud.java
@@ -3,7 +3,17 @@
  */
 public class Blud {
     /**
-     * Starts Blud and displays its name.
+     * Chains together a chat section using the input string array
+     *
+     * @param parts correspond to input string array in order of chaining
+     *
+     * @return section comprised of chained parts
+     */
+    public static String section(String[] parts) {
+        return String.join("\n", parts);
+    }
+    /**
+     * Starts Blud and displays its name, entry and exit greeting.
      *
      * @param args command-line arguments, which are currently unused
      */
@@ -16,6 +26,11 @@ public class Blud {
                 + "|  _ \\| | | | |/ __| |\n"
                 + "| |_) | | |_| | (__|_|\n"
                 + "|____/|_|\\__,_|\\___(_)\n";
-        System.out.println(banner);
+        String greeting = "Hey! This is Blud, what can I do for you today?";
+        String departure = "Thanks for the conversation, see you soon!";
+        String breakLine = "-------------------------------";
+        System.out.println(section(new String[]{breakLine, banner}));
+        System.out.println(section(new String[]{greeting, breakLine}));
+        System.out.println(section(new String[]{departure, breakLine}));
     }
 }
