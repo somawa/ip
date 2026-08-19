b4fcbdf2bcf21ade3c1283a3d995fb8197b9e753
Aw Guangyang Amos <aa.gyang88@gmail.com>
2026-08-19 12:28:39 +0800

    Level 2 changes to add user items to a list and display when prompted

--- src/main/java/Blud.java
+++ src/main/java/Blud.java
@@ -4,14 +4,54 @@ import java.util.Scanner;
  */
 public class Blud {
     /**
-     * Chains together a chat section using the input string array
-     *
-     * @param parts correspond to input string array in order of chaining
-     *
-     * @return section comprised of chained parts
+     * Mode enum to differentiate between list and simple printing
      */
-    public static String section(String[] parts) {
-        return "\t" + String.join("\n\t", parts);
+    public enum Mode {
+        SIMPLE,
+        LIST
+    }
+    /**
+     * Chains together a chat section using the input string array
+     * Prints the chained section
+     *
+     * @param header header to add as the first line of the section
+     * @param parts correspond to input string array in order of chaining
+     * @param footer footer to add at the last line of the section
+     * @param mode Mode enum for section formatting
+     */
+    public static void section(String header, String[] parts, String footer, Mode mode) {
+        if (header != null) {
+            System.out.println('\t' + header);
+        }
+        switch (mode) {
+            case SIMPLE:
+                System.out.println('\t' + String.join("\n\t", parts));
+                break;
+            case LIST:
+                listPrint(parts, '\t');
+                break;
+            default:
+                System.out.println("Invalid mode");
+        }
+        if (footer != null) {
+            System.out.println('\t' + footer);
+        }
+    }
+
+    /**
+     * Prints out list items with ids, with prefix at the front
+     *
+     * @param list list of items to print out
+     *
+     * @param prefix prefix to append to start of each item
+     */
+    public static void listPrint(String[] list, char prefix) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] ==  null) {
+                break;
+            }
+            System.out.println(prefix + Integer.toString(i + 1) + ". " + list[i]);
+        }
     }
     /**
      * Starts Blud and displays its name, entry and exit greeting.
@@ -30,15 +70,24 @@ public class Blud {
         String greeting = "Hey! This is Blud, what can I do for you today?";
         String departure = "Thanks for the conversation, see you soon!";
         String breakLine = "-------------------------------";
-        System.out.println(section(new String[]{breakLine, banner}));
-        System.out.println(section(new String[]{greeting, breakLine}));
         String userInput = "";
+        String listCommand = "list";
         String exitCommand = "bye";
+        String[] list = new String[100];
+        int nextListId = 0;
+
+        section(breakLine, new String[]{banner}, null, Mode.SIMPLE);
+        section(null, new String[]{greeting}, null, Mode.SIMPLE);
         userInput = scanner.nextLine();
         while (!exitCommand.equals(userInput)) {
-            System.out.println(section(new String[]{userInput, breakLine}));
+            if (listCommand.equals(userInput)) {
+                section(null, list, breakLine, Mode.LIST);
+            } else {
+                list[nextListId++] = userInput;
+                section(null, new String[]{("added: " + userInput)}, breakLine, Mode.SIMPLE);
+            }
             userInput = scanner.nextLine();
         }
-        System.out.println(section(new String[]{departure, breakLine}));
+        section(null, new String[]{departure}, breakLine, Mode.SIMPLE);
     }
 }
