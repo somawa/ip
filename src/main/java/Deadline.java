a0da1bc81ac2fa4eadc1f942bb4ace07027ab3d9
Aw Guangyang Amos <aa.gyang88@gmail.com>
2026-08-28 10:43:46 +0800

    Update Deadline.java to support datetime reading

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
-    private String deadline;
+//    private String deadline;
+    private LocalDateTime deadline;
     public Deadline (String[] parts) {
         if (parts[0].length() < 10) {
             throw new DeadlineException();
@@ -8,16 +11,25 @@ public class Deadline extends Task {
         }
         String mainDescription = parts[0].substring(9).strip();
         super(mainDescription);
-        this.deadline = parts[1].substring(3).strip();
+        this.deadline = DateUtils.parseInput(parts[1].substring(3).strip());
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), deadline);
+        return String.format(
+                "[D]%s (by: %s)",
+                super.toString(),
+                DateUtils.formatOutput(deadline)
+        );
     }
 
     @Override
     public String toStorageString() {
-        return String.format("D | %d | %s | %s", isDone() ? 1 : 0, getTaskDescription(), deadline);
+        return String.format(
+                "D | %d | %s | %s",
+                isDone() ? 1 : 0,
+                getTaskDescription(),
+                DateUtils.formatForStorage(deadline)
+        );
     }
 }
